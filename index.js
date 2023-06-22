b78caf5e66edae3f443894ec66d6b1ac933fad3a
Genevive-Mbesi <genevive.opondo@student.moringaschool.com>
2023-06-22 17:56:32 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove("#main");
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Hawa Maria is the champion";
+document.body.appendChild(newHeader);
